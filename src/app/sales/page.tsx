385c45cc5c81761fee99444795ebd22ae9559a56
Suyukee <diefuck@ya.ru>
2024-10-29 17:01:38 +0300

    Добавил ссылки для перехода в navbar и изменил стили

---
+++ src/app/sales/page.tsx
@@ -0,0 +1,10 @@
+import Header from '@/components/header';
+import styles from '@/styles/page.module.css';
+
+export default function SalesHome() {
+	return (
+		<div className={styles.page}>
+			<Header />
+		</div>
+	);
+}
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,27 +1,31 @@
+'use client';
+
 import Link from 'next/link';
 import styles from '@/styles/navbar.module.css';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
+	const pathname = usePathname();
 	return (
 		<nav className={styles.wrapper}>
 			<ul className={styles.list}>
-				<li className={styles.active}>
+				<li className={`${pathname === '/' && styles.active}`}>
 					<Link href="/">Преимущества Tele2</Link>
 				</li>
-				<li>
-					<Link href="/">Тарифы</Link>
+				<li className={`${pathname === '/tariffs' && styles.active}`}>
+					<Link href="/tariffs">Тарифы</Link>
 				</li>
-				<li>
-					<Link href="/">Акции и спецпредложения</Link>
+				<li className={`${pathname === '/sales' && styles.active}`}>
+					<Link href="/sales">Акции и спецпредложения</Link>
 				</li>
-				<li>
-					<Link href="/">Промотариф Tele2</Link>
+				<li className={`${pathname === '/promotariff' && styles.active}`}>
+					<Link href="/promotariff">Промотариф Tele2</Link>
 				</li>
-				<li>
-					<Link href="/">Технология eSIM</Link>
+				<li className={`${pathname === '/esim' && styles.active}`}>
+					<Link href="/esim">Технология eSIM</Link>
 				</li>
-				<li>
-					<Link href="/">Подключение нового абонента</Link>
+				<li className={`${pathname === '/newsubscriber' && styles.active}`}>
+					<Link href="/newsubscriber">Подключение нового абонента</Link>
 				</li>
 			</ul>
 		</nav>
